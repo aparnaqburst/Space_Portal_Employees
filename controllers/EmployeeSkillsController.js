aaa63a9733820ec0b590df1d8dd0d901dc2029bf
aparnaqburst <aparna@qburst.com>
2016-02-24 16:48:56 +0530

    latest changes

--- controllers/EmployeeSkillsController.js
+++ controllers/EmployeeSkillsController.js
@@ -2,6 +2,8 @@
     console.log('Skills');
     $scope.skillSet = [];
     $scope.isEdit = false;
+    $scope.isNewskill = false;
+    $scope.isRequired = false;
     $scope.fetchSkills = function () {
         EmployeeDetailsService.getSkillset().then
         (function (data) {
@@ -18,6 +20,7 @@
     }
     $scope.SaveSkills = function () {
         $scope.isEdit = !$scope.isEdit;
+        $scope.isNewskill = false;
     }
     $scope.setValue = function (item) {
         if ($scope.employeeSkills.indexOf(item.name) == -1)
@@ -26,5 +29,24 @@
             console.log("skills added", $scope.employeeSkills);
         }
         
+    }
+    $scope.addSkill = function () {
+        if (!$scope.isNewskill) {
+            $scope.isNewskill = true;
+        }
+        else {
+            if ($scope.validateForm.$valid) {
+                $scope.isNewskill = false;
+                $scope.isRequired = false;
+                if ($scope.employeeSkills.indexOf($scope.newSkill) == -1) {
+                    $scope.employeeSkills.push($scope.newSkill);
+                    $scope.newSkill = "";
+                }
+            }
+            else {
+                $scope.isRequired = true;
+            }
+        }
+        console.log("Add click fired");
     }
 });
